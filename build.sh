f66b134afabf6a4a9bd6effe44489bb78bc48503
Andy <andy.gura.jd@gmail.com>
2025-04-03 14:41:17 +0300

    Introduce reproducible build setup for the backend canister (#57)
    * introduced reproducible build setup for auction backend canister
    
    * upgraded tests to newer pic
    
    * fixed open handle in jest environment
    
    * fixed ignoring out/ directory

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+MOC_GC_FLAGS="" ## place any additional flags like compacting-gc, incremental-gc here
+MOC_FLAGS="$MOC_GC_FLAGS -no-check-ir --release --public-metadata candid:service --public-metadata candid:args"
+OUT=out/out_$(uname -s)_$(uname -m).wasm
+mops-cli build --lock --name out src/icrc1_auction_api.mo -- $MOC_FLAGS
+cp target/out/out.wasm $OUT
+ic-wasm $OUT -o $OUT shrink
+if [ -f did/icrc1_auction.did ]; then
+    echo "Adding icrc1_auction.did to metadata section."
+    ic-wasm $OUT -o $OUT metadata candid:service -f did/icrc1_auction.did -v public
+else
+    echo "icrc1_auction.did not found. Skipping metadata update."
+fi
+if [ "$compress" == "yes" ] || [ "$compress" == "y" ]; then
+  gzip -nf $OUT
+  sha256sum $OUT.gz
+else
+  sha256sum $OUT
+fi
