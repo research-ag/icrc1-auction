84ed8099933c6a8ecba25bedabab9546fc0b9622
andy <andy.gura.jd@gmail.com>
2026-02-21 12:47:14 +0200

    fixed generate script for linux systems

--- dfx_generate.sh
+++ dfx_generate.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
 # this script is for quick re-generation of declarations. Replaces @dfinity imports with @icp-sdk
 
 dfx generate crypto
@@ -5,7 +8,8 @@ dfx generate icrc1_auction_development
 dfx generate icrc1_ledger_mock
 
 find declarations -type f \( -name "*.ts" -o -name "*.js" \) -print0 | \
-  xargs -0 sed -i '' \
-    -e 's/@dfinity\/agent/@icp-sdk\/core\/agent/g' \
-    -e 's/@dfinity\/candid/@icp-sdk\/core\/candid/g' \
-    -e 's/@dfinity\/principal/@icp-sdk\/core\/principal/g'
+  xargs -0 sed -i.bak \
+    -e 's#@dfinity/agent#@icp-sdk/core/agent#g' \
+    -e 's#@dfinity/candid#@icp-sdk/core/candid#g' \
+    -e 's#@dfinity/principal#@icp-sdk/core/principal#g'
+find declarations -type f -name "*.bak" -delete
